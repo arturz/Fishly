1c74d6f5e888ce90da4c94fc682a43e38f34e2db
Artur <26014458+xout@users.noreply.github.com>
2020-03-30 12:15:09 +0200

    Fix fetch on localhost

--- app/src/utils/fetch.tsx
+++ app/src/utils/fetch.tsx
@@ -1,7 +1,9 @@
 import { stringify } from 'qs'
 
+const isLocalhost = ["localhost", "127.0.0.1", '[::1]'].includes(location.hostname)
+
 export default async (path: string, data?: object) => {
-  const body = await fetch(path, {
+  const body = await fetch(isLocalhost ? path : `/${path}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
